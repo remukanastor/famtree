17fe5d3bf87389b1c198ac929a1a499e28594965
Remukan Astor <82083002+remukanastor@users.noreply.github.com>
2026-03-31 11:09:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+const profileCard = document.getElementById("profileCard");
 
+fetch("data/family.json")
+.then(res => res.json())
+.then(json => {
+
+  const data = json.members;
+
+  const tree = new FamilyTree(document.getElementById("tree"), {
+
+    mouseScrool: FamilyTree.action.zoom,
+
+    nodeBinding: {
+      field_0: "name",
+      img_0: "photo"
+    },
+
+    nodeMouseClick: FamilyTree.action.none
+  });
+
+  tree.load(data);
+
+  tree.on('click', function(sender, args){
+
+    const person = args.node;
+
+    document.getElementById("profileName").innerText = person.name;
+    document.getElementById("profilePhoto").src = person.photo || "";
+    document.getElementById("profileInfo").innerText =
+      `Tahun Lahir: ${person.birth || '-'}\n${person.bio || ''}`;
+
+    profileCard.classList.remove("hidden");
+  });
+
+});
+
+document.getElementById("closeBtn").onclick = () =>
+  profileCard.classList.add("hidden");
